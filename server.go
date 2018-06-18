d691952fea1bff3c0f72ddd418f081eb62118a5d
smileisak <ikaboubi@gmail.com>
2018-06-18 13:39:22 +0200

    Kubernetes Demo: Done

--- server.go
+++ server.go
@@ -14,20 +14,28 @@ var format = logging.MustStringFormatter(
 	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
-func main() {
+func initLogger() {
 	backend := logging.NewLogBackend(os.Stderr, "", 0)
 	backendFormatter := logging.NewBackendFormatter(backend, format)
-
 	backendLeveled := logging.AddModuleLevel(backend)
 	backendLeveled.SetLevel(logging.INFO, "")
-
 	logging.SetBackend(backendLeveled, backendFormatter)
+}
 
-	log.Notice("Server Listing on 0.0.0.0:8080")
+func main() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	initLogger()
+	log.Notice("Server Listeing on 0.0.0.0:8080")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Notice("Request Handled: URI %s", r.URL.Path)
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Pod handling this request: %s \n", hostname)
+		log.Notice("Request Handled: URI %s", r.URL.Path)
 	})
 	http.ListenAndServe(":8080", nil)
 }
